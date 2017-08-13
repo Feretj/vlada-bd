08bfb9ce50b986ea74f6685e30336882c128da1b
feretj <g.sergey.wolf@gmail.com>
2017-08-14 02:15:56 +0300

    Start coding

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById("root"));
+class BirthdayApp extends React.Component {
+  render() {
+    return <Password />;
+  }
+}
+
+class Password extends React.Component {
+  render() {
+    return (
+      <div style={{ height: "100%", width: "100%", display: "flex", alignItems: "center", justifyContent: "center"}}>
+        <div style={{}}>
+          <input placeholder="type password" />{" "}
+        </div>
+      </div>
+    );
+  }
+}
+
+console.log("Hello, use window.Start_Vlada_hacking()");
+window.Start_Vlada_hacking = function() {
+  alert("ok")
+}
+
+ReactDOM.render(<BirthdayApp />, document.getElementById("root"));
