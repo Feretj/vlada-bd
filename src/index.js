a6f263528befc24b2e092eb0caefe3b14dc99dfc
feretj <g.sergey.wolf@gmail.com>
2017-09-11 00:26:08 +0300

    Make changing screens

--- src/index.js
+++ src/index.js
@@ -1,27 +1,83 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const screens = {
+  happybirthday: "Привет",
+  vlada: null,
+  sergey: "wtf",
+  "yeah, hack me ;)": "Oohhhhhh yeee"
+};
+
 class BirthdayApp extends React.Component {
+  state = { password: "" };
+  onPasswordChange = password => this.setState({ password });
+  componentWillMount = () => {
+    window.Start_Vlada_hacking = () => {
+      this.setState({ password: "yeah, hack me ;)" });
+    };
+  };
   render() {
-    return <Password />;
-  }
-}
+    const props = {
+      password: this.state.password,
+      onPasswordChange: this.onPasswordChange
+    };
 
-class Password extends React.Component {
-  render() {
-    return (
-      <div style={{ height: "100%", width: "100%", display: "flex", alignItems: "center", justifyContent: "center"}}>
-        <div style={{}}>
-          <input placeholder="type password" />{" "}
+    let mainComponent = <Password {...props} />;
+    if (screens[this.state.password]) {
+      const component = screens[this.state.password];
+      mainComponent = (
+        <div
+          style={{
+            height: "100vh",
+            width: "100vw",
+            display: "flex"
+          }}
+        >
+          <PasswordHeader {...props} />
+          <component />{" "}
         </div>
-      </div>
-    );
+      );
+    }
+    return mainComponent;
   }
 }
 
-console.log("Hello, use window.Start_Vlada_hacking()");
-window.Start_Vlada_hacking = function() {
-  alert("ok")
-}
+const Password = ({ password, onPasswordChange }) => (
+  <div
+    style={{
+      height: "100vh",
+      width: "100vw",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center"
+    }}
+  >
+    <div>
+      <input placeholder="type password" value={password} />
+      <input type="button" />
+    </div>
+  </div>
+);
+
+const PasswordHeader = ({ password, onPasswordChange }) => (
+  <div
+    style={{
+      height: "10%",
+      width: "100%",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      flexDirection: "column"
+    }}
+  >
+    Пароль для этой страницы, запомни, чтоб вернуться
+    <div>
+      <input placeholder="type password" value={password} />
+      <input type="button" />
+    </div>
+  </div>
+);
+
+console.log("Hello, if you are Vlade use window.Start_Vlada_hacking()");
 
 ReactDOM.render(<BirthdayApp />, document.getElementById("root"));
